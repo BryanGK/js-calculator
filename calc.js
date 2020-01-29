6f5ad84cfc8bd2a111cc56f396e21c3e89c25a35
Trevor Mearns <spiral272@gmail.com>
2020-01-29 12:33:44 +0900

    digits now displaying in UI on click but only one at a time

--- calc.js
+++ calc.js
@@ -2,18 +2,17 @@ const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const display = document.querySelector('.display');
 let result = 0;
-
-function displayNumber(number) {
-
-}
+console.log(digits);
 //detect and render the digit that has been pressed
-
 for(let i = 0; i < digits.length; i++) {
     let theDigit = digits[i];
-
     theDigit.addEventListener('click', (e) => {
-        console.log(theDigit);
-        theDigit = e.target.value;
-        display.innerHTML = theDigit;
+        console.log(theDigit.innerHTML);
+        console.log('the digit is ', theDigit);
+        
+        const firstNumber = `<span> ${theDigit.innerHTML} </span>`
+        display.innerHTML = firstNumber;
     })
-}
+    };
+
+    
