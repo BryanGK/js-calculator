988c5eebd192c9017212b431a2c4ce532afeef9a
Trevor Mearns <spiral272@gmail.com>
2020-01-29 13:30:19 +0900

    stuck on how to append multiple digits and how to calculate

--- calc.js
+++ calc.js
@@ -1,18 +1,47 @@
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const display = document.querySelector('.display');
-let result = 0;
-console.log(digits);
+display.innerHTML = 0;
+let firstNumber = null;
+let secondNumber = null;
+
+
 //detect and render the digit that has been pressed
 for(let i = 0; i < digits.length; i++) {
     let theDigit = digits[i];
     theDigit.addEventListener('click', (e) => {
-        console.log(theDigit.innerHTML);
-        console.log('the digit is ', theDigit);
         
-        const firstNumber = `<span> ${theDigit.innerHTML} </span>`
+        let firstNumber = `<span> ${theDigit.innerHTML} </span>`
         display.innerHTML = firstNumber;
+        
     })
+
     };
 
+    function calculate() {
+        for(let i = 0; i < operators.length; i++) {
+            let theOperator = operators[i];
+
+            theOperator.addEventListener('click', (e) =>{
+                console.log('This is the operator', theOperator.innerHTML);
+            })
+            
+
+            if(theOperator === "add") {
+               return firstNumber + secondNumber;
+
+
+            } 
+    }};
+
+    calculate();
+//detect and render operator that has been pressed
+/*for(let i = 0; i < operators.length; i++) {
+    let theOperator = operators[i];
+    theOperator.addEventListener('click', (e) => {
+        
+        const operator = `<span> ${theOperator.innerHTML} </span>`
+        display.innerHTML = operator;
+    })
+    };*/
     
